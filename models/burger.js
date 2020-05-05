9c1c8c74ce7b97aee682eb79dc479ec4f0b3f890
Ben Fung <benfung1996@gmail.com>
2020-05-04 21:05:25 -0700

    replacing orm reference

--- models/burger.js
+++ models/burger.js
@@ -3,26 +3,26 @@ var orm = require("../config/orm.js");
 // The code that will call the ORM functions using burger specific input for the ORM.
 var burger = {
     // Display all burgers in the db.
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function(res) {
+    findAll: function(cb) {
+        orm.findAll("burgers", function(res) {
             cb(res);
         });
     },
     // Add a new burger to the db.
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
     // Change the devoured status to true.
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
             cb(res);
         });
     },
     // Delete a burger from the db.
-    deleteOne: function(condition, cb) {
-        orm.deleteOne("burgers", condition, function(res) {
+    destroy: function(condition, cb) {
+        orm.destroy("burgers", condition, function(res) {
             cb(res);
         });
     }
